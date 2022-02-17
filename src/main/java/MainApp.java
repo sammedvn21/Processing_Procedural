d8ef7301211f90f1b10b6997b978face85c76ee0
Sammed Nandrekar <sammedvnandrekar@gmail.com>
2022-02-17 17:51:17 +0530

    Final Commit

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -1,7 +1,7 @@
 import processing.core.PApplet;
 public class MainApp extends PApplet {
 
-    public static final int WIDTH = 1000;
+    public static final int WIDTH = 960;
     public static final int HEIGHT = 480;
     int x=0;
 
